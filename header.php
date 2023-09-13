e05cf25eef45e23579d5bc09bfc4e4148da52d5e
iurakenji <iurakenji@gmail.com>
2023-09-13 12:41:15 -0300

    .

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
         <div class="d-flex justify-content-between align-items-center">
             <a href="home.php"><i class="lni lni-invest-monitor logo text-white"></i></a>
             <div class="d-flex justify-content-between align-items-center">
-                Olá,   <a class="link-offset-2 link-underline link-underline-opacity-0 link-underline-opacity-0-hover" href="usuario.php"><img class="img rounded-circle" src="<?=$_SESSION['imagem'];?>" alt="Foto" style="width:48px;height:48px;"><?=$_SESSION['nome'];?></i></a>
+                Olá,   <a style="margin-left: 10px;" class="link-offset-2 link-underline link-underline-opacity-0 link-underline-opacity-0-hover" href="usuario.php"><img  class="img rounded-circle" src="<?=$_SESSION['imagem'];?>" alt="Foto" style="width:48px;height:48px; margin-right: 10px;"><?=$_SESSION['nome'];?></i></a>
             </div>
         </div>
     </div>
